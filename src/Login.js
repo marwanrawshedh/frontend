0538cd45eb0c6014c8f5d02c00b27f67f816778e
marwan raswashdeh <marwansalem55@yahoo.com>
2023-04-25 13:38:45 +0300

    new modification

--- src/Login.js
+++ src/Login.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Card from 'react-bootstrap/Card';
-import './Login.css';
-import Loginbotton from './LoginBotton'
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "react-bootstrap/Card";
+import "./Login.css";
+import Loginbotton from "./LoginBotton";
 class Login extends React.Component {
   render() {
-    return(
-      <Card style={{ width: '18rem' }}>
+    console.log("fff");
+    return (
+      <Card style={{ width: "18rem" }}>
         <Card.Body>
           <Card.Title>Log In</Card.Title>
-          <Card.Text>
-            Click Below to Log In
-          </Card.Text>
+          <Card.Text>Click Below to Log In</Card.Text>
           {/* TODO: add a `LoginButton` component here that will log the user in with Auth0 */}
-          <Loginbotton/>
+          <Loginbotton />
         </Card.Body>
       </Card>
-    )
+    );
   }
 }
 
